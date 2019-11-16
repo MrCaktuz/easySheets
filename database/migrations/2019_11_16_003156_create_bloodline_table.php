c0eb9fe263ea34027c8c7e690a2d1e6fc746ceb7
Mathieu Claessens <mathieu@referencefitness.com>
2019-11-16 04:46:57 +0100

    Migrations - Naming lineage to bloodline

--- database/migrations/2019_11_16_003156_create_bloodline_table.php
+++ database/migrations/2019_11_16_003156_create_bloodline_table.php
@@ -4,11 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateLineageTable extends Migration {
+class CreateBloodlineTable extends Migration {
 
 	public function up()
 	{
-		Schema::create('lineage', function(Blueprint $table) {
+		Schema::create('bloodline', function(Blueprint $table) {
 			$table->increments('id');
 			$table->string('name')->unique();
 		});
@@ -16,6 +16,6 @@ class CreateLineageTable extends Migration {
 
 	public function down()
 	{
-		Schema::drop('lineage');
+		Schema::drop('bloodline');
 	}
 }
--- database/migrations/2019_11_16_003156_create_character_bloodline_table.php
+++ database/migrations/2019_11_16_003156_create_character_bloodline_table.php
@@ -4,18 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCharacterLineageTable extends Migration {
+class CreateCharacterBloodlineTable extends Migration {
 
 	public function up()
 	{
-		Schema::create('character_lineage', function(Blueprint $table) {
-			$table->integer('lineage_id')->unique()->unsigned();
+		Schema::create('character_bloodline', function(Blueprint $table) {
+			$table->integer('bloodline_id')->unique()->unsigned();
 			$table->integer('character_id')->unique()->unsigned();
 		});
 	}
 
 	public function down()
 	{
-		Schema::drop('character_lineage');
+		Schema::drop('character_bloodline');
 	}
 }
