f95ec2a54e88a3d70f0721ec46c69be363c82242
Mathieu Claessens <mathieu@referencefitness.com>
2019-11-24 17:32:09 +0100

    Header - First Draw

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,30 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img/favicon/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/favicon/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/favicon/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('img/favicon/site.webmanifest') }}">
+    <link rel="mask-icon" href="{{ asset('img/favicon/safari-pinned-tab.svg') }}" color="#1e3a4c">
+    <meta name="apple-mobile-web-app-title" content="{{ config('app.name', 'EasySheets') }}">
+    <meta name="application-name" content="{{ config('app.name', 'EasySheets') }}">
+    <meta name="msapplication-TileColor" content="#dc6c11">
+    <meta name="theme-color" content="#dc6c11">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'EasySheets') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600|Vollkorn:400,600&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
